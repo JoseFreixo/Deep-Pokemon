98d5039affaf1cf01c6520c7195100ad455d9c2d
JoseFreixo <josefreixo15@gmail.com>
2020-01-10 16:25:56 +0000

    Most recent version, still not working

--- bot.py
+++ bot.py
@@ -10,13 +10,21 @@ from poke_env.player.trainable_player import TrainablePlayer
 from poke_env.player.utils import cross_evaluate
 from poke_env.environment.battle import Battle
 from poke_env.player_configuration import PlayerConfiguration
-from poke_env.server_configuration import LocalhostServerConfiguration
+from poke_env.server_configuration import LocalhostServerConfiguration, ServerConfiguration
 
-from typing import Dict
+from typing import Dict, Optional
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
+
+from tensorflow.python.keras import backend as K
+import tensorflow as tf
+
+# config = tf.compat.v1.ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = tf.compat.v1.Session(config=config)
+# K.set_session(session)
 
 class MaxDamagePlayer(Player):
     def choose_move(self, battle):
@@ -31,12 +39,48 @@ class MaxDamagePlayer(Player):
             return self.choose_random_move(battle)
 
 class PokeAgent(TrainablePlayer):
-    def init_model(self):
+    def __init__(
+        self,
+        player_configuration: PlayerConfiguration,
+        *,
+        avatar: Optional[int] = None,
+        battle_format: str,
+        log_level: Optional[int] = None,
+        max_concurrent_battles: int = 1,
+        model=None,
+        server_configuration: ServerConfiguration,
+        start_listening: bool = True,
+    ) -> None:
+    
+        config = tf.compat.v1.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.session = tf.compat.v1.Session(config=config)
+        self.graph = tf.compat.v1.get_default_graph()
+        K.set_session(self.session)
+        with self.graph.as_default():
+            with self.session.as_default():
+                self.graph.finalize()
+
+        super(PokeAgent, self).__init__(
+            player_configuration=player_configuration, 
+            avatar=avatar, 
+            battle_format=battle_format,
+            log_level=log_level,
+            max_concurrent_battles=max_concurrent_battles,
+            model=model, 
+            server_configuration=server_configuration,
+            start_listening=start_listening
+        )
+
+    @staticmethod
+    def init_model():
         model = Sequential()
         model.add(Dense(110, activation="relu", input_shape=(110,)))
         model.add(Dense(110, activation="relu"))
         model.add(Dense(9, activation="softmax"))
+        model._make_predict_function()
         model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        
         return model
 
     def action_to_move(self, action, battle: Battle):
@@ -60,10 +104,12 @@ class PokeAgent(TrainablePlayer):
         # ----- Add opponent moves info ----- #
         state = np.append(state, bu.getMovesInfo(battle.opponent_active_pokemon, battle.active_pokemon))
         return state
-        
+    
     def state_to_action(self, state: np.array, battle: Battle):
         print("Antes do predict")
-        actions = self.model.predict(state)
+        with self.graph.as_default():
+            with self.session.as_default():
+                actions = self.model.predict_on_batch(state)
         print("----------- ACTIONS HERE -----------")
         print(actions)
         pass
@@ -78,7 +124,7 @@ async def main():
     # We define two player configurations.
     player_1_configuration = PlayerConfiguration("Agent player", None)
     player_2_configuration = PlayerConfiguration("Random player", None)
-
+    
     # We create the corresponding players.
     agent_player = PokeAgent(
         player_configuration=player_1_configuration,
